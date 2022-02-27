6df0670551876bacd83c620e908fb4f6520e1f01
victorjatoba <victorjatoba10@gmail.com>
2022-02-27 16:29:41 -0300

    add generic map function

---
+++ generics/map.js
@@ -0,0 +1,5 @@
+"use strict";
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = ['foo', 'bar', 'baz'];
+let incrementedArray = arr1.map(n => n + n);
+console.log(incrementedArray);
---
+++ generics/map.ts
@@ -0,0 +1,6 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = ['foo', 'bar', 'baz'];
+
+let incrementedArray = arr1.map(n => n + n);
+
+console.log(incrementedArray);
