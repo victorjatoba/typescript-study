21830848f5eb89cfd55876e633dcc46653b829d6
victorjatoba <victorjatoba10@gmail.com>
2022-02-27 17:09:05 -0300

    add map first example with interface

--- generics/map.ts
+++ generics/map.ts
@@ -1,6 +1,16 @@
 let arr1 = [1, 2, 3, 4, 5];
-let arr2 = ['foo', 'bar', 'baz'];
+let arr2 = ["foo", "bar", "baz"];
 
-let incrementedArray = arr1.map(n => n + n);
+interface User {
+  name: string;
+  age: number;
+}
+
+let incrementedArray = arr1.map<User>((n) => {
+  return {
+    name: "foo",
+    age: n + 1,
+  };
+});
 
 console.log(incrementedArray);
---
+++ stl/ipList.js
@@ -0,0 +1,13 @@
+"use strict";
+function isAllowed(ips, ip) {
+    return !ips.includes(ip);
+}
+const blockedList = [
+    "192.168.10.1",
+    "192.168.10.2",
+    "192.168.10.3",
+    "192.168.10.4",
+];
+console.log(isAllowed(blockedList, "192.168.10.1")); // false
+console.log(isAllowed(blockedList, "192.168.10.4")); // false
+console.log(isAllowed(blockedList, "192.168.10.9")); // true
