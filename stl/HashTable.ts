c2f49b10aa48c37f69ddfa09a815a7c522f494e8
victorjatoba <vmgjatoba@gmail.com>
2024-06-18 10:10:49 +0100

    improve hash table solution

--- stl/HashTable.ts
+++ stl/HashTable.ts
@@ -1,26 +1,25 @@
-interface HashTable<T> {
+interface HashTable {
+    [key: string]: number
+}
+
+let walletSimple: HashTable = {}
+walletSimple["BTC"]=33
+console.log(walletSimple["BTC"])
+
+interface HashTableGeneric<T> {
     [key: string]: T;
 }
 
-let wallet: HashTable<number> = {};
+let wallet: HashTableGeneric<number> = {};
 wallet["BTC"] = 0.03
 wallet["ETH"] = 0.01
 console.log(wallet["BTC"])
 
-interface HashTableNumber<T> {
-    [key: number]: T;
-}
-
-let walletByValue: HashTableNumber<string> = {}
-walletByValue[0.03] = "BTC"
-walletByValue[0.01] = "ETH"
-console.log(walletByValue[0.03])
-
-type HashTableGeneric<T extends string | number | symbol, S> = {
+type HashTableFullGeneric<T extends string | number | symbol, S> = {
     [key in T]: S;
 };
 
-let genericWallet: HashTableGeneric<string, string> = {}
+let genericWallet: HashTableFullGeneric<string, string> = {}
 genericWallet["hot wallet"] = "ALS$K31FJO291-!!2923"
 genericWallet["cold wallet"] = "089GJ)_(UN8M9U890-@*!"
 console.log(genericWallet["hot wallet"])
